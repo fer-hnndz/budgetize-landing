28303b8ccb086d55ea243a3e8aabcf057561c72a
fer-hnndz <jfernandohernandez28@gmail.com>
2025-05-14 10:40:31 -0600

    Revert "add mailchimp popup form"
    This reverts commit 31a02b650bb23ca58da22e690489e9ae5c9117ba.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Budgetize",
@@ -21,20 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="mcjs"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,h,i,m,p){
-                m=c.createElement(h),p=c.getElementsByTagName(h)[0];
-                m.async=1;m.src=i;p.parentNode.insertBefore(m,p);
-              })(document,"script","https://chimpstatic.com/mcjs-connected/js/users/65b6be58aa0fcd8c541241f1f/85b0469d05f9df96de3e46807.js");
-            `,
-          }}
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
